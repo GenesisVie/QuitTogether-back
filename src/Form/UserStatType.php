a4b5fc2264caf90e8990a463f13cee8fca64ec7d
nrgix2 <antoine.mousset@ynov.com>
2020-04-07 15:49:37 +0200

    big update

---
+++ src/Form/UserStatType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\UserStat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserStatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('moneyEconomised')
+            ->add('cigarettesSaved')
+            ->add('lifetimeSaved')
+            ->add('title')
+            ->add('statistic')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => UserStat::class,
+        ]);
+    }
+}
